be32ef2f6f6c7027a430d5b1a99c71df9739dc86
Bành Hảo Toàn <toanbh@hasaki.vn>
2026-07-29 17:05:07 +0700

    feat: update Go development environment with additional tools

--- nix/packages/go.nix
+++ nix/packages/go.nix
@@ -1,13 +1,17 @@
 { pkgs }:
 
 with pkgs; [
+  deadcode
   go
+  govulncheck
   gopls
   golangci-lint
   grpcurl
   mockgen
+  nilaway
   protoc-gen-go
   protoc-gen-go-grpc
+  staticcheck
   gotools
   gotests
   go-migrate
